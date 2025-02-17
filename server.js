90a57df0b616ba570c902348d5825639235990ee
Arayaphong Traisopon <arayaphong.traisopon@krungsri.com>
2025-02-17 21:41:46 +0700

    Refactor socket event handlers for improved readability and maintainability

--- server.js
+++ server.js
@@ -35,72 +35,79 @@ function updatePlayersInRoom(roomId) {
     }
 }
 
+// Event handlers
+function handleJoin(socket, { username, roomId, isScrumMaster, isVoter }) {
+    socket.join(roomId);
+    if (!rooms.has(roomId)) {
+        rooms.set(roomId, { users: new Map(), votes: new Map(), currentStory: '' });
+    }
+    rooms.get(roomId).users.set(socket.id, { username, isScrumMaster, isVoter });
+    console.log(`${username} joined room ${roomId}`);
+    updatePlayersInRoom(roomId);
+}
+
+function handleVote(socket, { username, roomId, value }) {
+    const room = rooms.get(roomId);
+    if (room && room.votes) {
+        room.votes.set(username, value);
+        updatePlayersInRoom(roomId);
+    } else {
+        console.error(`Room ${roomId} not found or votes not initialized`);
+    }
+}
+
+function handleRevealVotes(socket, { roomId }) {
+    const room = rooms.get(roomId);
+    if (room) {
+        const votes = Array.from(room.votes, ([username, value]) => ({ username, value }));
+        io.to(roomId).emit('votes_revealed', { votes });
+    }
+}
+
+function handleStartVoting(socket, { roomId, story }) {
+    const room = rooms.get(roomId);
+    if (room) {
+        room.currentStory = story;
+        room.votes.clear();
+        io.to(roomId).emit('voting_started', { story });
+    }
+}
+
+function handleCancelVoting(socket, { roomId }) {
+    const room = rooms.get(roomId);
+    if (room) {
+        room.currentStory = '';
+        io.to(roomId).emit('voting_cancelled');
+    }
+}
+
+function handleDisconnect(socket) {
+    console.log('Client disconnected');
+    rooms.forEach((room, roomId) => {
+        if (room.users.has(socket.id)) {
+            const username = room.users.get(socket.id).username;
+            room.users.delete(socket.id);
+            room.votes.delete(username);
+            console.log(`${username} left room ${roomId}`);
+            updatePlayersInRoom(roomId);
+            if (room.users.size === 0) {
+                rooms.delete(roomId);
+                console.log(`Room ${roomId} deleted`);
+            }
+        }
+    });
+}
+
+// Socket connection setup
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    socket.on('join', ({ username, roomId, isScrumMaster, isVoter }) => {
-        socket.join(roomId);
-        if (!rooms.has(roomId)) {
-            rooms.set(roomId, { users: new Map(), votes: new Map(), currentStory: '' });
-        }
-        rooms.get(roomId).users.set(socket.id, { username, isScrumMaster, isVoter });
-        console.log(`${username} joined room ${roomId}`);
-        updatePlayersInRoom(roomId);
-    });
-
-    socket.on('vote', ({ username, roomId, value }) => {
-        const room = rooms.get(roomId);
-        if (room && room.votes) {
-            room.votes.set(username, value);
-            updatePlayersInRoom(roomId);
-        } else {
-            console.error(`Room ${roomId} not found or votes not initialized`);
-        }
-    });
-
-
-    socket.on('reveal_votes', ({ roomId }) => {
-        const room = rooms.get(roomId);
-        if (room) {
-            const votes = Array.from(room.votes, ([username, value]) => ({ username, value }));
-            io.to(roomId).emit('votes_revealed', { votes });
-        }
-    });
-
-    socket.on('start_voting', ({ roomId, story }) => {
-        const room = rooms.get(roomId);
-        if (room) {
-            room.currentStory = story;
-            room.votes.clear();
-            io.to(roomId).emit('voting_started', { story });
-        }
-    });
-
-    socket.on('cancel_voting', ({ roomId }) => {
-        const room = rooms.get(roomId);
-        if (room) {
-            room.currentStory = '';
-            // Don't clear votes here
-            io.to(roomId).emit('voting_cancelled');
-        }
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Client disconnected');
-        rooms.forEach((room, roomId) => {
-            if (room.users.has(socket.id)) {
-                const username = room.users.get(socket.id).username;
-                room.users.delete(socket.id);
-                room.votes.delete(username);
-                console.log(`${username} left room ${roomId}`);
-                updatePlayersInRoom(roomId);
-                if (room.users.size === 0) {
-                    rooms.delete(roomId);
-                    console.log(`Room ${roomId} deleted`);
-                }
-            }
-        });
-    });
+    socket.on('join', (data) => handleJoin(socket, data));
+    socket.on('vote', (data) => handleVote(socket, data));
+    socket.on('reveal_votes', (data) => handleRevealVotes(socket, data));
+    socket.on('start_voting', (data) => handleStartVoting(socket, data));
+    socket.on('cancel_voting', (data) => handleCancelVoting(socket, data));
+    socket.on('disconnect', () => handleDisconnect(socket));
 });
 
 const PORT = process.env.PORT || 3000;
